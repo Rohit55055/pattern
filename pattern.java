0610150727125ccac3c17dfdc478f1ce99efb959
Rohit55055 <114055350+Rohit55055@users.noreply.github.com>
2022-09-23 10:24:00 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int n = 4;
+    int m = 5;
+    
+    //outerloop
+    for(int i=1; i<=n; i++) {
+      //innerloop
+      for(int j=1; j<=m; j++) {
+        System. out. print("*");
+      }
+      System. out. println();
+      
+    }
+   }
+ }
